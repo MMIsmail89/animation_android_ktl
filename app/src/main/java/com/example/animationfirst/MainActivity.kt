c1ff476feea937206e7d4e703532ba9f09260fd2
Magdi <86538068+MMIsmail89@users.noreply.github.com>
2023-09-11 00:10:42 +0300

    add modifications in some comments!!

--- app/src/main/java/com/example/animationfirst/MainActivity.kt
+++ app/src/main/java/com/example/animationfirst/MainActivity.kt
@@ -134,13 +134,13 @@ class MainActivity : AppCompatActivity() {
 
     private fun showingNewStart() {
 
-        /*
-        First, you’re going to need some local variables to hold state that we will need in the ensuing code.
-        Specifically, you’ll need:
-            a reference to the star field ViewGroup (which is just the parent of the current star view).
-            the width and height of that container (which you will use to calculate the end translation values for our falling stars).
-            the default width and height of our star (which you will later alter with a scale factor to get different-sized stars).
-         */
+            /*
+            First, you’re going to need some local variables to hold state that we will need in the ensuing code.
+            Specifically, you’ll need:
+                a reference to the star field ViewGroup (which is just the parent of the current star view).
+                the width and height of that container (which you will use to calculate the end translation values for our falling stars).
+                the default width and height of our star (which you will later alter with a scale factor to get different-sized stars).
+             */
 
         // >> 1 - Get a reference to the parent container:
         val container = binding?.mainIvStar?.parent as ViewGroup
@@ -149,15 +149,12 @@ class MainActivity : AppCompatActivity() {
         val containerW = container.width
         val containerH = container.height
 
-
-        //
-
-        /*
-        Create a new View to hold the star graphic.
-        Because the star is a VectorDrawable asset, use an AppCompatImageView,
-        which has the ability to host that kind of resource.
-        Create the star and add it to the background container.
-         */
+            /*
+            Create a new View to hold the star graphic.
+            Because the star is a VectorDrawable asset, use an AppCompatImageView,
+            which has the ability to host that kind of resource.
+            Create the star and add it to the background container.
+             */
 
         // >> 3- Create a new star (newStar) as an AppCompatImageView:
         val newStar = AppCompatImageView(this)
@@ -175,12 +172,12 @@ class MainActivity : AppCompatActivity() {
         // >> 6- Add the new star to the container:
         container.addView(newStar)
 
-        /*
-        Set the size of the star.
-         Modify the star to have a random size, from .1x to 1.6x of its default size.
-          Use this scale factor to change the cached width/height values,
-          because you will need to know the actual pixel height/width for later calculations.
-         */
+            /*
+            Set the size of the star.
+             Modify the star to have a random size, from .1x to 1.6x of its default size.
+              Use this scale factor to change the cached width/height values,
+              because you will need to know the actual pixel height/width for later calculations.
+             */
 
         // >> 7- Size and position the new star
         newStar.scaleX = Math.random().toFloat() * 1.5f + .1f
@@ -197,26 +194,26 @@ class MainActivity : AppCompatActivity() {
         // starW = starW?.times(newStar.scaleX)
         // starH = starH?.times(newStar.scaleY)
 
-        /*
-        Now position the new star. Horizontally, it should appear randomly somewhere from the left edge to the right edge.
-        This code uses the width of the star to position it
-        from half-way off the screen on the left (-starW / 2) to half-way off the screen on the right
-        (with the star positioned at (containerW - starW / 2). The vertical positioning of the star will be handled later in the actual animation code.
-         */
+            /*
+            Now position the new star. Horizontally, it should appear randomly somewhere from the left edge to the right edge.
+            This code uses the width of the star to position it
+            from half-way off the screen on the left (-starW / 2) to half-way off the screen on the right
+            (with the star positioned at (containerW - starW / 2). The vertical positioning of the star will be handled later in the actual animation code.
+             */
 
         if (starW != null) {
             newStar.translationX = Math.random().toFloat() *
                     containerW - starW / 2
         }
 
-        /*
-        Creating animators for star rotation and falling
+            /*
+            Creating animators for star rotation and falling
 
-        The star should rotate as it falls downwards.
-        You’ve already seen one way to animate two properties together,
-        using PropertyValuesHolder, in the previous task on scaling.
+            The star should rotate as it falls downwards.
+            You’ve already seen one way to animate two properties together,
+            using PropertyValuesHolder, in the previous task on scaling.
+             */
 
-         */
         // >> 8- Create two animations for the new star:
 
         // create two animators, along with their interpolators:
@@ -224,14 +221,14 @@ class MainActivity : AppCompatActivity() {
             -starH!!, containerH + starH!!
         )
 
-        /*
-        Running the animations in parallel with AnimatorSet
+            /*
+            Running the animations in parallel with AnimatorSet
 
-        >> Create the AnimatorSet and add the child animators to it (along with information to play them in parallel).
-        The default animation time of 300 milliseconds is too quick to enjoy the falling stars,
-        so set the duration to a random number between 500 and 2000 milliseconds,
-        so stars fall at different speeds.
-         */
+            >> Create the AnimatorSet and add the child animators to it (along with information to play them in parallel).
+            The default animation time of 300 milliseconds is too quick to enjoy the falling stars,
+            so set the duration to a random number between 500 and 2000 milliseconds,
+            so stars fall at different speeds.
+             */
 
         mover.interpolator = AccelerateInterpolator(1f)
         val rotator = ObjectAnimator.ofFloat(newStar, View.ROTATION,
@@ -248,12 +245,12 @@ class MainActivity : AppCompatActivity() {
 
 
         // >> 9 - Add an animation listener to remove the new star from the container when the animation ends:
-        /*
-        Once newStar has fallen off the bottom of the screen,
-        it should be removed from the container.
-        Set a simple listener to wait for the end of the animation and remove it.
-        Then start the animation.
-         */
+            /*
+            Once newStar has fallen off the bottom of the screen,
+            it should be removed from the container.
+            Set a simple listener to wait for the end of the animation and remove it.
+            Then start the animation.
+             */
 
         set.addListener(object : AnimatorListenerAdapter() {
             override fun onAnimationEnd(animation: Animator) {
